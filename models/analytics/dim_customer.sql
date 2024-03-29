15e8e2930b75c20a9ecb7ba370d4b573bf03ce5c
christiandang <phuongnamgdc@gmail.com>
2024-03-29 21:54:22 +0700

    0107c

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -8,6 +8,8 @@ WITH dim_customer__source AS(
   SELECT 
     customer_id AS customer_key
     , customer_name AS customer_name
+    , customer_category_id AS customer_category_key
+    , buying_group_id AS buying_group_key
   FROM dim_customer__source
 )
 
@@ -15,12 +17,21 @@ WITH dim_customer__source AS(
   SELECT 
     CAST(customer_key AS INTEGER) AS customer_key
     , CAST(customer_name AS STRING) AS customer_name
+    , CAST(customer_category_key AS INTEGER) AS customer_category_key
+    , CAST(buying_group_key AS INTEGER) AS buying_group_key
   FROM dim_customer__rename_column
 )
 
 SELECT 
-  customer_key
-  , customer_name
-FROM dim_customer__cast_type
-
+  dim_customer.customer_key
+  , dim_customer.customer_name
+  , dim_customer.customer_category_key
+  , fact_sales_customer_categories.customer_category_name
+  , fact_sales_buying_groups.buying_group_key
+  , buying_group_name
+FROM dim_customer__cast_type AS dim_customer
+LEFT JOIN {{ ref('stg_fact_sales_customer_categories') }} AS fact_sales_customer_categories
+  ON dim_customer.customer_category_key = fact_sales_customer_categories.customer_category_key
+LEFT JOIN {{ ref('stg_fact_sales_buying_groups') }} AS fact_sales_buying_groups
+  ON dim_customer.buying_group_key = fact_sales_buying_groups.buying_group_key 
 
