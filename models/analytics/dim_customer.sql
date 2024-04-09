5371ae89daf0b468bb9415d9f126ab512a9ee15d
christiandang <phuongnamgdc@gmail.com>
2024-04-09 23:54:56 +0700

    dim_cus_model

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -11,6 +11,17 @@ WITH dim_customer__source AS(
     , customer_category_id AS customer_category_key
     , buying_group_id AS buying_group_key
     , is_on_credit_hold AS is_on_credit_hold_boolean
+    , is_statement_sent AS is_statement_sent_boolean
+    , credit_limit
+    , standard_discount_percentage
+    , payment_days
+    , account_opened_date
+    , delivery_method_id AS delivery_method_key
+    , delivery_city_id AS delivery_city_key
+    , postal_city_id AS postal_city_key
+    , primary_contact_person_id AS primary_contact_person_key
+    , alternate_contact_person_id AS alternate_contact_person_key
+    , bill_to_customer_id AS bill_to_customer_key
   FROM dim_customer__source
 )
 
@@ -21,6 +32,17 @@ WITH dim_customer__source AS(
     , CAST(customer_category_key AS INTEGER) AS customer_category_key
     , CAST(buying_group_key AS INTEGER) AS buying_group_key
     , CAST(is_on_credit_hold_boolean AS BOOLEAN) AS is_on_credit_hold_boolean
+    , CAST(is_statement_sent_boolean AS BOOLEAN) AS is_statement_sent_boolean
+    , CAST(credit_limit AS FLOAT64) AS credit_limit
+    , CAST(standard_discount_percentage AS FLOAT64) AS standard_discount_percentage
+    , CAST(payment_days AS INTEGER) AS payment_days
+    , CAST(account_opened_date AS DATE) AS account_opened_date
+    , CAST(delivery_method_key AS INTEGER) AS delivery_method_key
+    , CAST(delivery_city_key AS INTEGER) AS delivery_city_key
+    , CAST(postal_city_key AS INTEGER) AS postal_city_key
+    , CAST(primary_contact_person_key AS INTEGER) AS primary_contact_person_key
+    , CAST(alternate_contact_person_key AS INTEGER) AS alternate_contact_person_key
+    , CAST(bill_to_customer_key AS INTEGER) AS bill_to_customer_key
   FROM dim_customer__rename_column
 )
 
@@ -33,6 +55,12 @@ WITH dim_customer__source AS(
       WHEN is_on_credit_hold_boolean IS NULL THEN 'Undefined'
       ELSE 'Invalid'
       END AS is_on_credit_hold
+      , CASE 
+      WHEN is_statement_sent_boolean IS TRUE THEN 'Statement Sent'
+      WHEN is_statement_sent_boolean IS FALSE THEN 'Not Statement Sent'
+      WHEN is_statement_sent_boolean IS NULL THEN 'Undefined'
+      ELSE 'Invalid'
+      END AS is_statement_sent
   FROM dim_customer__cast_type
 )
 
@@ -43,6 +71,17 @@ WITH dim_customer__source AS(
     , is_on_credit_hold
     , customer_category_key
     , buying_group_key
+    , is_statement_sent
+    , credit_limit
+    , standard_discount_percentage
+    , payment_days
+    , account_opened_date
+    , delivery_method_key
+    , delivery_city_key
+    , postal_city_key
+    , primary_contact_person_key
+    , alternate_contact_person_key
+    , bill_to_customer_key
   FROM dim_customer__convert_boolean
   
   UNION ALL
@@ -52,6 +91,17 @@ WITH dim_customer__source AS(
     , 'Invalid' AS is_on_credit_hold
     , -1 AS customer_category_key
     , -1 AS buying_group_key
+    , 'Invalid' AS is_statement_sent
+    , NULL AS credit_limit
+    , NULL AS standard_discount_percentage
+    , NULL AS payment_days
+    , NULL AS account_opened_date
+    , -1 AS delivery_method_key
+    , -1 AS delivery_city_key
+    , -1 AS postal_city_key
+    , -1 AS primary_contact_person_key
+    , -1 AS alternate_contact_person_key
+    , -1 AS bill_to_customer_key
 
   UNION ALL
   SELECT
@@ -60,6 +110,17 @@ WITH dim_customer__source AS(
     , 'Undefined' AS is_on_credit_hold
     , 0 AS customer_category_key
     , 0 AS buying_group_key
+    , 'Invalid' AS is_statement_sent
+    , NULL AS credit_limit
+    , NULL AS standard_discount_percentage
+    , NULL AS payment_days
+    , NULL AS account_opened_date
+    , -1 AS delivery_method_key
+    , -1 AS delivery_city_key
+    , -1 AS postal_city_key
+    , -1 AS primary_contact_person_key
+    , -1 AS alternate_contact_person_key
+    , -1 AS bill_to_customer_key
 )
 
 SELECT 
@@ -69,10 +130,42 @@ SELECT
   , dim_customer.customer_category_key
   , COALESCE(dim_customer_categories.customer_category_name,'Invalid') AS customer_category_name 
   , dim_buying_groups.buying_group_key
-  , COALESCE(dim_buying_groups.buying_group_name,'Invalid') AS buying_group_name 
+  , COALESCE(dim_buying_groups.buying_group_name,'Invalid') AS buying_group_name
+  , dim_customer.is_statement_sent
+  , dim_customer.credit_limit
+  , dim_customer.standard_discount_percentage
+  , dim_customer.payment_days
+  , dim_customer.account_opened_date
+  , dim_customer.delivery_method_key
+  , dim_delivery_method.delivery_method_name
+  , dim_customer.delivery_city_key
+  , dim_delivery_city.city_name AS delivery_city_name
+  , dim_delivery_city.state_province_key AS delivery_state_province_key
+  , dim_delivery_city.state_province_name AS delivery_state_province_name
+  , dim_customer.postal_city_key
+  , dim_postal_city.city_name AS postal_city_name
+  , dim_postal_city.state_province_key AS postal_state_province_key
+  , dim_postal_city.state_province_name AS postal_state_province_name
+  , dim_customer.primary_contact_person_key
+  , dim_primary_contact_person.full_name AS primary_contact_person_name
+  , dim_customer.alternate_contact_person_key
+  , dim_alternate_contact_person.full_name AS alternate_contact_person_name
+  , dim_customer.bill_to_customer_key
+  , dim_bill_to_customer.bill_to_customer_name
 FROM dim_customer__add_undefined_record AS dim_customer
 LEFT JOIN {{ ref('stg_dim_customer_categories') }} AS dim_customer_categories
   ON dim_customer.customer_category_key = dim_customer_categories.customer_category_key
 LEFT JOIN {{ ref('stg_dim_buying_groups') }} AS dim_buying_groups
   ON dim_customer.buying_group_key = dim_buying_groups.buying_group_key 
-
+LEFT JOIN {{ ref('stg_dim_delivery_method') }} AS dim_delivery_method
+  ON dim_customer.delivery_method_key = dim_delivery_method.delivery_method_key
+LEFT JOIN {{ ref('stg_dim_city') }} AS dim_delivery_city
+  ON dim_customer.delivery_city_key = dim_delivery_city.city_key
+LEFT JOIN {{ ref('stg_dim_city') }} AS dim_postal_city
+  ON dim_customer.postal_city_key = dim_postal_city.city_key
+LEFT JOIN {{ ref('dim_person') }} AS dim_primary_contact_person
+  ON dim_customer.primary_contact_person_key = dim_primary_contact_person.person_key
+LEFT JOIN {{ ref('dim_person') }} AS dim_alternate_contact_person
+  ON dim_customer.alternate_contact_person_key = dim_alternate_contact_person.person_key
+LEFT JOIN {{ ref('stg_dim_bill_to_customer') }} AS dim_bill_to_customer
+  ON dim_customer.bill_to_customer_key = dim_bill_to_customer.bill_to_customer_key
